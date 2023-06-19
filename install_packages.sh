ab4b7d821111a92be02922cf00ff77abaa562c1b
Jonathan Hernandez <136935368+JH-UCB@users.noreply.github.com>
2023-06-18 17:45:36 -0700

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,4 @@
-  #!/bin/bash
+#!/bin/bash
 
 # Check if the script is running as root
 if [ "$EUID" -ne 0 ]
@@ -12,6 +12,9 @@ apt update -y && apt upgrade -y
 
 # Install the required packages
 echo "Installing required packages..."
-apt install -y make darkhttpd recode tar
+apt install -y make darkhttpd recode tar curl
+
+echo "Downloading the tarball file from the specified URL..."
+curl -L -O https://github.com/UC-Berkeley-I-School/MIDS-1D-Computing-Basics/website.tar.gz
 
 echo "All done!"
